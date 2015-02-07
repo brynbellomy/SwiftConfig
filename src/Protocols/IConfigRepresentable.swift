9c2ab5693e3cea6dd0a396b6dfc94d6aa6a4a930
bryn austin bellomy <bryn.bellomy@gmail.com>
2015-02-07 00:00:26 -0600

    Added 'configValue' property to IConfigRepresentable protocol

--- src/Protocols/IConfigRepresentable.swift
+++ src/Protocols/IConfigRepresentable.swift
@@ -12,31 +12,39 @@ import Foundation
 public protocol IConfigRepresentable
 {
     typealias ConfigValueType
-    class func fromConfigValue(configValue:ConfigValueType) -> Self?
+
+    class func fromConfigValue(configValue: ConfigValueType) -> Self?
+    var configValue: ConfigValueType { get }
 }
 
 
 extension String: IConfigRepresentable {
     public static func fromConfigValue(configValue:String) -> String? { return configValue }
+    public var configValue: String { return self }
 }
 
 extension Int: IConfigRepresentable {
     public static func fromConfigValue(configValue:Int) -> Int? { return configValue }
+    public var configValue: Int { return self }
 }
 
 extension Float: IConfigRepresentable {
     public static func fromConfigValue(configValue:Float) -> Float? { return configValue }
+    public var configValue: Float { return self }
 }
 
 extension Double: IConfigRepresentable {
     public static func fromConfigValue(configValue:Double) -> Double? { return configValue }
+    public var configValue: Double { return self }
 }
 
 extension Bool: IConfigRepresentable {
     public static func fromConfigValue(configValue:Bool) -> Bool? { return configValue }
+    public var configValue: Bool { return self }
 }
 
 extension CGFloat: IConfigRepresentable {
+    public var configValue: CGFloat { return self }
     public static func fromConfigValue(configValue:CGFloat) -> CGFloat? {
         return configValue
     }
