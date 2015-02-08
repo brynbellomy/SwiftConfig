374c5413cd05d76201323a11dff33d44c076d258
bryn austin bellomy <bryn.bellomy@gmail.com>
2015-02-08 16:21:51 -0600

    OSColor (NS/UIColor typealias) is now conformant to new IConfigRepresentable requirements

--- src/Extensions/Color+Config.swift
+++ src/Extensions/Color+Config.swift
@@ -25,6 +25,17 @@ public typealias OSColor = NSColor
 
 extension OSColor: IConfigRepresentable
 {
+    public var configValue: String {
+        var red   : CGFloat = 0
+        var green : CGFloat = 0
+        var blue  : CGFloat = 0
+        var alpha : CGFloat = 0
+
+        getRed(&red, green:&green, blue:&blue, alpha:&alpha)
+
+        return "rgba(\(red), \(green), \(blue), \(alpha))"
+    }
+
     public class func fromConfigValue(configValue:String) -> Self?
     {
         if configValue.hasPrefix("rgba") {
